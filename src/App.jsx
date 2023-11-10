5c95aa2249f3ea70b52b6e90363899669fba00a5
d0w0b <d0w0b@d0w0bs-MacBook-Air.local>
2023-11-10 03:14:19 -0800

    set up  the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Signout from './pages/Signout'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Home from './pages/Home'
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
+  return (<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/signin" element={<Signin />}/>
+        <Route path="/signout" element={<Signout />}/>
+
+        
+      </Routes>
+      </BrowserRouter>
+
   )
 }
 
