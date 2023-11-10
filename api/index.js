af16ab78e1dd0dd4381f316e4296f18b56b9e8f7
d0w0b <d0w0b@d0w0bs-MacBook-Air.local>
2023-11-10 07:54:03 -0800

    connect tp DB

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
-import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connect to MongoDB");
+  })
+  .catch((err) => {
+    console.log(`Error ${err}`);
+  });
 
 const app = express();
 const port = 3000;
 
-
-
-
-
 app.listen(port, () => {
-
-    console.log(`connect to ${port}`)
-})
+  console.log(`connect to ${port}`);
+});
