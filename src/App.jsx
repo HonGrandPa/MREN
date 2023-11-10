a3b6128867dc741595faaafe02b4982557022546
d0w0b <d0w0b@d0w0bs-MacBook-Air.local>
2023-11-10 03:58:20 -0800

    Adding Header with the link to specific page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route} from "react-router-dom"
-import Signout from './pages/Signout'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import Signin from './pages/Signin'
-import Home from './pages/Home'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signout from "./pages/Signout";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App = () => {
-  return (<BrowserRouter>
+  return (
+    <BrowserRouter>
+    <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/profile" element={<Profile />}/>
-        <Route path="/signin" element={<Signin />}/>
-        <Route path="/signout" element={<Signout />}/>
-
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signout" element={<Signout />} />
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
