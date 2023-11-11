03a21f67afeaa8d4926ac0d15fe3fba1cf842fc3
d0w0b <d0w0b@d0w0bs-MacBook-Air.local>
2023-11-11 01:26:12 -0800

    added SIGH UP function and update the authcontroller file

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,32 +1,39 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import { errorHandler } from "../utils/error.js";
 
-
-
 export const signup = async (req, res, next) => {
+  const { userName, email, password } = req.body; //req.body is from client
 
-    const { userName, email, password } = req.body; //req.body is from client
+  // const bcryptPassword = bcrypt.hashSync(password, 10)
 
-    const bcryptPassword = bcrypt.hashSync(password, 10);
-
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      console.log(`Fail to hash password due to Error: ${err}`);
+      next(err);
+    }
     const newUser = new User({
-        userName,
-        email,
-        password: bcryptPassword
+      userName,
+      email,
+      password: hash,
     });
 
-    await newUser.save()
-        .then(result => {
-            console.log(`new user: ${newUser.userName} has been saved`);
-            res.status(201).json({ message: "New User has been saved" })
-        })
-        .catch(err => {
-            console.log(`Error Occur : ${err}`);
-            next(err);
-            // next(errorHandler(550, 'eeror from the function'));
-        });
+    newUser
+      .save()
+      .then((result) => {
+        console.log(`new user: ${newUser.userName} has been saved`);
+        res.status(201).json({ message: "New User has been saved" });
+      })
+      .catch((err) => {
+        console.log(`Error Occur : ${err}`);
+        next(err);
+        // next(errorHandler(550, 'eeror from the function'));
+      });
+  });
+};
 
-
-
-}
+// const newUser = new User({
+//     userName,
+//     email,
+//     password: bcryptPassword
+// });
--- api/utils/error.js
+++ api/utils/error.js
@@ -1,5 +1,4 @@
 export const errorHandler = (statusCode, message) => {
-
     const error = new Error(); // <- create an arr object
     error.statusCode =  statusCode;
     error.message = message
