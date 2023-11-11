8863494f7cf111264cb188ef986fd30b7643b206
d0w0b <d0w0b@d0w0bs-MacBook-Air.local>
2023-11-10 20:28:48 -0800

    created controller and routes folder and use app.use from index.js test and it works contollers folder contain the deliver info and rout contain the routing logic and lastly index.js run the apu server

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js"
 
 mongoose
   .connect(process.env.MONGO)
@@ -14,6 +15,9 @@ mongoose
 
 const app = express();
 const port = 3000;
+
+// "/api/user is a link and userRouter is exported from user.route with fun of "get""
+app.use("/api/user", userRouter)
 
 app.listen(port, () => {
   console.log(`connect to ${port}`);
