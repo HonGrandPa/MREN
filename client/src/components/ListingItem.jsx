e6052b6a24cd050e00a3247487d3616b85f0f685
d0w0b <d0w0b@d0w0bs-MacBook-Air.local>
2023-11-15 23:09:31 -0800

    Add show more listings functionality

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -10,7 +10,7 @@ export default function ListingItem({ listing }) {
             listing.imageUrls[0] ||
             '#'
           }
-          alt='listing cover'
+          alt='listing-img'
           className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
         />
         <div className='p-3 flex flex-col gap-2 w-full'>
